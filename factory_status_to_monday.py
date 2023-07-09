24feba8b7c827a6475653cc538c9d297deae0039
Efrat <efrat@senecio-robotics.com>
2023-07-09 19:16:11 +0300

    make some changes, now we should have a good foundation

--- factory_status_to_monday.py
+++ factory_status_to_monday.py
@@ -1,162 +1,105 @@
+import csv
 import serial
 import time
-import requests
-import json
-import datetime
+from datetime import datetime, timedelta
+import os
+# Define your temperature range
+TEMPERATURE_RANGE = (27.0, 31.0)
 
+# Initialize your min and max temperatures to None
+min_temperature = None
+max_temperature = None
 
-def create_item(adultsTemp,adultsHumid,laraveTemp,measurmentID):
-    apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzNzkyNTIxMSwidWlkIjoxNTg2MzcxMiwiaWFkIjoiMjAyMy0wMi0xNlQyMjoyNzoxMi41NzFaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6Njk5MzQzNCwicmduIjoidXNlMSJ9.CeSxYrLQxn-8jN8O-TQaMk0YlSlmW-wlPuYEWjOAiG4'
+def temperature_out_of_range(temperature, temperature_range):
+    return not (temperature_range[0] <= temperature <= temperature_range[1])
 
-    apiUrl = "https://api.monday.com/v2"
-    headers = {"Authorization": apiKey}
+def handle_temperature_out_of_range():
+    # This function will be called when the temperature is out of the specified range
+    # You can put any code you want here
+    print("Temperature is out of range!")
 
-    now = datetime.datetime.now().strftime("%m-%d %H:%M")
-
-    query = 'mutation ($myItemName: String!, $columnVals: JSON!) { create_item (board_id:3984889513, item_name:$myItemName, column_values:$columnVals) { id } }'
-    vars = {
-        'myItemName': now,
-        'columnVals': json.dumps({
-            'numeric': adultsTemp,
-            'numeric1': adultsHumid,
-            'numeric6': laraveTemp,
-            'text4': measurmentID
-        })
-    }
-
-    data = {'query': query, 'variables': vars}
-    r = requests.post(url=apiUrl, json=data, headers=headers)  # make request
-    print(r.json())
-
-def searchByColumnValue(column_value):
-    # Replace *** with your actual API key
-    apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzNzkyNTIxMSwidWlkIjoxNTg2MzcxMiwiaWFkIjoiMjAyMy0wMi0xNlQyMjoyNzoxMi41NzFaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6Njk5MzQzNCwicmduIjoidXNlMSJ9.CeSxYrLQxn-8jN8O-TQaMk0YlSlmW-wlPuYEWjOAiG4'
-
-    apiUrl = "https://api.monday.com/v2"
-    headers = {"Authorization": apiKey}
-
-
-    # Define the query and variables
-    query = 'query ($columnValue: String!){items_by_column_values(board_id:3984889513, column_id:"text4", column_value:$columnValue){id name}}'
-
-    variables = {
-        'columnValue': column_value
-    }
-
-    # Define the request parameters
-    data = {'query': query, 'variables': variables}
-
-    # Send the request
-    response = requests.post(url=apiUrl, json=data, headers=headers)
-
-    print(response.json())
-
-    # Parse the response
-    data = response.json()
-
-    # Get the list of item IDs and names
-    item_data = data['data']['items_by_column_values']
-    item_ids = [item['id'] for item in item_data]
-    item_names = [item['name'] for item in item_data]
-
-    # Print the item IDs and names
-    print('Item IDs:', item_ids)
-    print('Item names:', item_names)
-
-    if item_ids:
-        item_id = str(item_ids[0])
-        print(f"item_id: '{item_id}'")
-    else:
-        print("item_ids list is empty")
-
-    if item_ids:
-        return(item_id)
-
-def moveItemToGroup(itemID,groupId):
-
-    apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzNzkyNTIxMSwidWlkIjoxNTg2MzcxMiwiaWFkIjoiMjAyMy0wMi0xNlQyMjoyNzoxMi41NzFaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6Njk5MzQzNCwicmduIjoidXNlMSJ9.CeSxYrLQxn-8jN8O-TQaMk0YlSlmW-wlPuYEWjOAiG4'
-    apiUrl = "https://api.monday.com/v2"
-    headers = {"Authorization": apiKey}
-
-    #query2 = 'query{boards(ids: 3984889513){groups{id title}}}' #to find group id
-
-    query1 = 'mutation {move_item_to_group (item_id: %s, group_id: "%s") {id}}' % (itemID,groupId)
-
-    variables = {
-            'itemId': itemID,
-            'groupId':groupId}
-
-
-    data = {'query': query1}#,'variables':variables}
-    r = requests.post(url=apiUrl, json=data, headers=headers)  # make request
-    print(r.json())
-
-def create_group():
-    apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzNzkyNTIxMSwidWlkIjoxNTg2MzcxMiwiaWFkIjoiMjAyMy0wMi0xNlQyMjoyNzoxMi41NzFaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6Njk5MzQzNCwicmduIjoidXNlMSJ9.CeSxYrLQxn-8jN8O-TQaMk0YlSlmW-wlPuYEWjOAiG4'
-
-    apiUrl = "https://api.monday.com/v2"
-    headers = {"Authorization": apiKey}
-
-    now = datetime.datetime.now().strftime("%d/%m/%y - %H:%M")
-    group_name = now
-    query = 'mutation {{ create_group (board_id: 3984889513, group_name: "{}", position:"1") {{ id }} }}'.format(group_name)
-
-    data = {'query': query}
-    r = requests.post(url=apiUrl, json=data, headers=headers)  # make request
-    print(r.json())
-
-def delete_item(itemId):
-    apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzNzkyNTIxMSwidWlkIjoxNTg2MzcxMiwiaWFkIjoiMjAyMy0wMi0xNlQyMjoyNzoxMi41NzFaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6Njk5MzQzNCwicmduIjoidXNlMSJ9.CeSxYrLQxn-8jN8O-TQaMk0YlSlmW-wlPuYEWjOAiG4'
-
-    apiUrl = "https://api.monday.com/v2"
-    headers = {"Authorization": apiKey}
-
-
-    query = 'mutation { delete_item (item_id: %s) { id } }' % itemId
-
-    data = {'query': query}
-    r = requests.post(url=apiUrl, json=data, headers=headers)  # make request
-    print(r.json())
-
+def write_csv(file_path, data):
+    with open(file_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
 
 def main():
+    global min_temperature, max_temperature
 
-    measurmentID = 0
+    # Initialize the last recorded time to one hour ago
+    last_recorded_time = datetime.now() - timedelta(minutes=30)
 
+    file_path = 'recordings.csv'
+    start_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+    # Create the CSV file if it doesn't exist and write the headers
+    if not os.path.isfile(file_path):
+        write_csv(file_path, [['Start', 'Time', 'Adults Room Temperature', 'Adults Room Humidity', 'Larvae Room Temperature', 'Max Temperature', 'Min Temperature'],
+                              [start_time, '', '', '', '', '', '']])
 
     ser = serial.Serial('COM9', 9600)  # Open the serial port
-
 
     time.sleep(2)  # Wait for the Arduino to initialize
 
     while True:
-        # Read the data from the serial port
-        data = ser.readline().decode().strip()
+        try:
+            # Read the data from the serial port
+            data = ser.readline().decode().strip()
 
-        # Split the data into temperature and humidity values
-        values = data.split(',')
-        adults_temperature = float(values[0])
-        adults_humidity = float(values[1])
-        larvae_temperature = float(values[2])
+            # Split the data into temperature and humidity values
+            values = data.split(',')
+            if len(values) != 3:
+                print(f"Unexpected data format: {data}")
+                continue
 
-        # Print the values
-        print('Adults Room Temperature: {:.2f}°C'.format(adults_temperature))
-        print('Adults Room Humidity: {:.2f}%'.format(adults_humidity))
-        print('Larvae Room Temperature: {:.2f}°C'.format(larvae_temperature))
+            try:
+                adults_temperature = float(values[0])
+                adults_humidity = float(values[1])
+                larvae_temperature = float(values[2])
+            except ValueError:
+                print(f"Unable to convert data to float: {values}")
+                continue
 
-        if measurmentID >= 8065: ##monday limit for items per board
-            delete_item(searchByColumnValue(str(measurmentID-8065)))
-        measurmentID += 1
-        create_group()
-        time.sleep(5) ##
-        create_item(str(adults_temperature),str(adults_humidity),str(larvae_temperature),str(measurmentID))
+            # Update the min and max temperatures if necessary
+            temperatures = [adults_temperature, larvae_temperature]
+            min_temperature = min(temperatures) if min_temperature is None else min(min_temperature, min(temperatures))
+            max_temperature = max(temperatures) if max_temperature is None else max(max_temperature, max(temperatures))
 
+            # Get the current time
+            now = datetime.now()
 
+            # Print the current time and measurements
+            print(f'[{now.strftime("%Y-%m-%d %H:%M:%S")}] Current measurements:')
+            print(f'Adults Room Temperature: {adults_temperature:.2f}°C')
+            print(f'Adults Room Humidity: {adults_humidity:.2f}%')
+            print(f'Larvae Room Temperature: {larvae_temperature:.2f}°C')
 
-        time.sleep(15)  # Wait for 5 seconds before reading again
+            # Check if the temperature is out of range and call the appropriate function if it is
+            if temperature_out_of_range(adults_temperature, TEMPERATURE_RANGE) or temperature_out_of_range(larvae_temperature, TEMPERATURE_RANGE):
+                handle_temperature_out_of_range()
 
+            # Load the existing data
+            with open(file_path, 'r') as file:
+                data = list(csv.reader(file))
 
+            # Update min and max temperature in the first row
+            data[1][5] = max_temperature
+            data[1][6] = min_temperature
 
-if __name__ == '__main__':
+            # Write to CSV every 30 minutes
+            if (now - last_recorded_time) >= timedelta(minutes=30):
+                # Append the new row
+                data.append(['', now.strftime("%Y-%m-%d %H:%M:%S"), adults_temperature, adults_humidity, larvae_temperature, '', ''])
+                last_recorded_time = now
+
+            # Write the data back to the CSV
+            write_csv(file_path, data)
+
+        except serial.SerialException as e:
+            print(f"Error reading from serial port: {e}")
+            time.sleep(10)  # Sleep before trying to read again
+
+        time.sleep(5*60)  # Wait for 5 minutes before reading again
+
+if __name__ == "__main__":
     main()
---
+++ monday.py
@@ -0,0 +1,113 @@
+
+def create_item(adultsTemp,adultsHumid,laraveTemp,measurmentID):
+    apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzNzkyNTIxMSwidWlkIjoxNTg2MzcxMiwiaWFkIjoiMjAyMy0wMi0xNlQyMjoyNzoxMi41NzFaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6Njk5MzQzNCwicmduIjoidXNlMSJ9.CeSxYrLQxn-8jN8O-TQaMk0YlSlmW-wlPuYEWjOAiG4'
+
+    apiUrl = "https://api.monday.com/v2"
+    headers = {"Authorization": apiKey}
+
+    now = datetime.datetime.now().strftime("%m-%d %H:%M")
+
+    query = 'mutation ($myItemName: String!, $columnVals: JSON!) { create_item (board_id:3984889513, item_name:$myItemName, column_values:$columnVals) { id } }'
+    vars = {
+        'myItemName': now,
+        'columnVals': json.dumps({
+            'numeric': adultsTemp,
+            'numeric1': adultsHumid,
+            'numeric6': laraveTemp,
+            'text4': measurmentID
+        })
+    }
+
+    data = {'query': query, 'variables': vars}
+    r = requests.post(url=apiUrl, json=data, headers=headers)  # make request
+    print(r.json())
+
+def searchByColumnValue(column_value):
+    # Replace *** with your actual API key
+    apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzNzkyNTIxMSwidWlkIjoxNTg2MzcxMiwiaWFkIjoiMjAyMy0wMi0xNlQyMjoyNzoxMi41NzFaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6Njk5MzQzNCwicmduIjoidXNlMSJ9.CeSxYrLQxn-8jN8O-TQaMk0YlSlmW-wlPuYEWjOAiG4'
+
+    apiUrl = "https://api.monday.com/v2"
+    headers = {"Authorization": apiKey}
+
+
+    # Define the query and variables
+    query = 'query ($columnValue: String!){items_by_column_values(board_id:3984889513, column_id:"text4", column_value:$columnValue){id name}}'
+
+    variables = {
+        'columnValue': column_value
+    }
+
+    # Define the request parameters
+    data = {'query': query, 'variables': variables}
+
+    # Send the request
+    response = requests.post(url=apiUrl, json=data, headers=headers)
+
+    print(response.json())
+
+    # Parse the response
+    data = response.json()
+
+    # Get the list of item IDs and names
+    item_data = data['data']['items_by_column_values']
+    item_ids = [item['id'] for item in item_data]
+    item_names = [item['name'] for item in item_data]
+
+    # Print the item IDs and names
+    print('Item IDs:', item_ids)
+    print('Item names:', item_names)
+
+    if item_ids:
+        item_id = str(item_ids[0])
+        print(f"item_id: '{item_id}'")
+    else:
+        print("item_ids list is empty")
+
+    if item_ids:
+        return(item_id)
+
+def moveItemToGroup(itemID,groupId):
+
+    apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzNzkyNTIxMSwidWlkIjoxNTg2MzcxMiwiaWFkIjoiMjAyMy0wMi0xNlQyMjoyNzoxMi41NzFaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6Njk5MzQzNCwicmduIjoidXNlMSJ9.CeSxYrLQxn-8jN8O-TQaMk0YlSlmW-wlPuYEWjOAiG4'
+    apiUrl = "https://api.monday.com/v2"
+    headers = {"Authorization": apiKey}
+
+    #query2 = 'query{boards(ids: 3984889513){groups{id title}}}' #to find group id
+
+    query1 = 'mutation {move_item_to_group (item_id: %s, group_id: "%s") {id}}' % (itemID,groupId)
+
+    variables = {
+            'itemId': itemID,
+            'groupId':groupId}
+
+
+    data = {'query': query1}#,'variables':variables}
+    r = requests.post(url=apiUrl, json=data, headers=headers)  # make request
+    print(r.json())
+
+def create_group():
+    apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzNzkyNTIxMSwidWlkIjoxNTg2MzcxMiwiaWFkIjoiMjAyMy0wMi0xNlQyMjoyNzoxMi41NzFaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6Njk5MzQzNCwicmduIjoidXNlMSJ9.CeSxYrLQxn-8jN8O-TQaMk0YlSlmW-wlPuYEWjOAiG4'
+
+    apiUrl = "https://api.monday.com/v2"
+    headers = {"Authorization": apiKey}
+
+    now = datetime.datetime.now().strftime("%d/%m/%y - %H:%M")
+    group_name = now
+    query = 'mutation {{ create_group (board_id: 3984889513, group_name: "{}", position:"1") {{ id }} }}'.format(group_name)
+
+    data = {'query': query}
+    r = requests.post(url=apiUrl, json=data, headers=headers)  # make request
+    print(r.json())
+
+def delete_item(itemId):
+    apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzNzkyNTIxMSwidWlkIjoxNTg2MzcxMiwiaWFkIjoiMjAyMy0wMi0xNlQyMjoyNzoxMi41NzFaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6Njk5MzQzNCwicmduIjoidXNlMSJ9.CeSxYrLQxn-8jN8O-TQaMk0YlSlmW-wlPuYEWjOAiG4'
+
+    apiUrl = "https://api.monday.com/v2"
+    headers = {"Authorization": apiKey}
+
+
+    query = 'mutation { delete_item (item_id: %s) { id } }' % itemId
+
+    data = {'query': query}
+    r = requests.post(url=apiUrl, json=data, headers=headers)  # make request
+    print(r.json())
